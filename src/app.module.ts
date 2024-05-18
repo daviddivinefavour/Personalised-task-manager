4edce4e2629f299d0d806baa1d38c1e058127cc4
Divinefavour David <blazer0022@gmail.com>
2024-05-18 13:26:45 +0100

    initial configurations set.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import configuration from './config/configuration';
+import { ConfigModule } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    SequelizeModule.forRoot(configuration.database),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,38 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { WinstonModule } from 'nest-winston';
+import instance from './shared/utils/logger';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+    logger: WinstonModule.createLogger({
+      instance: instance,
+    }),
+  });
+  app.setGlobalPrefix('api/v1');
+
+  const allowedOrigins = ['*'];
+  app.enableCors({
+    origin: allowedOrigins,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
+  if (['development', 'staging'].includes(NODE_ENV)) {
+    const config = new DocumentBuilder()
+      .setTitle('Niyo todo API service')
+      .setDescription('The backend service call for Niyo todo  backend')
+      .setVersion('1.0')
+      .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api/v1/docs', app, document);
+  }
+  await app.listen(process.env.PORT || 3500);
 }
 bootstrap();
